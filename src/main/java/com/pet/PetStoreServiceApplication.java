f9a3fe9c21fb9b7579417ebed7ee62abf0b68c6c
nidhi <nidhi@192.168.2.21>
2017-06-14 22:16:02 -0400

    updating settings

--- src/main/java/com/pet/PetStoreServiceApplication.java
+++ src/main/java/com/pet/PetStoreServiceApplication.java
@@ -1,12 +1,22 @@
 package com.pet;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PetStoreServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetStoreServiceApplication.class, args);
+	}
+	
+	@Bean
+	public ServletRegistrationBean h2servletRegistration() {
+	    ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+	    registration.addUrlMappings("/console/*");
+	    return registration;
 	}
 }
